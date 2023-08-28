0c516185d3ad2518ab2358c1a1360ba3a728bcf1
rikcy <bishtsarthak80@gmail.com>
2023-08-28 16:03:50 +0530

    first commit

---
+++ hellow.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"hellow sir"<<endl;
+    int a;
+    cout<<"Enter the number"<<endl;
+    cin>>a;
+    cout<<"Your num is :"<<a<<endl;
+          for (int i=0;i<a;i++)
+          {
+            cout<<i;
+          }
+    return 0;
+}
---
+++ ric.c++
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello sir" << endl;
+    int a;
+    cout << "Enter the number" << endl;
+    cin >> a;
+    for (int i = 0; i < a; i++) {  
+        cout << i << endl;
+    }
+    return 0;
+}
